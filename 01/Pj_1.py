21261c26339471adf9b3048173a372577eb653c3
Alx <strizhakov.s.33@gmail.com>
2020-10-10 09:55:48 +0300

    initial commit

---
+++ 01/Pj_1.py
@@ -0,0 +1,14 @@
+st = "Пример строки"
+print(st, type(st))
+number = 10
+print(number, type(number))
+number_with_dot = 2.5
+print(number_with_dot, type(number_with_dot))
+boo = True
+print(boo, type(boo))
+no = None
+print(no, type(no))
+st1 = input("Введите что нибудь")
+print("Вы ввели", st1,"тип: ", type(st1))
+
+
